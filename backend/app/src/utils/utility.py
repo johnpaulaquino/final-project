0313f4d19a0ac55d1b550f6a3750a4dac93d288b
johnpaulaquino <johnpaul.dev72@gmail.com>
2026-05-08 07:03:32 +0800

    fixed cors

--- backend/app/src/utils/utility.py
+++ backend/app/src/utils/utility.py
@@ -13,7 +13,7 @@ from app.src.schema.products_schema import ProductStatusSchema
 
 
 class Utility:
-    
+
     @staticmethod
     def capitalize_first_letters(words: str):
         try:
@@ -21,41 +21,40 @@ class Utility:
                 return ""
             # split words
             split_words = words.split(" ")
-            
+
             formatted_word = []
             for word in split_words:
                 word = word.strip().lower()
                 word = word[:1].capitalize() + word[1:]
                 formatted_word.append(word)
-            
+
             return " ".join(formatted_word)
         except Exception as e:
             raise e
-    
+
     @staticmethod
     def config_successful_response(sign_in_type: int):
-        
+
         config = {1: SuccessfulResponseSchema(message="Verification code sent to your email.",
                                               action="EMAIL_SENT",
                                               status_message="accepted",
                                               status_code=status.HTTP_202_ACCEPTED),
-                  
+
                   2: SuccessfulResponseSchema(message="Verification code sent to your email.",
                                               action="EMAIL_SENT",
                                               status_message="accepted",
                                               status_code=status.HTTP_202_ACCEPTED), }
-        
+
         return config.get(sign_in_type)
-    
+
     @staticmethod
     def verify_otp(otp_code_from_redis, otp_code):
         if not otp_code_from_redis:
             raise DomainOTPExpiredError(message="Your code is expired. Please request a new one.", )
-        
+
         if otp_code_from_redis != otp_code:
-            
             raise DomainOTPInvalidError(message="Incorrect OTP code.")
-    
+
     @staticmethod
     def find_data_from_list(key: str, dict_data: list, list_of_data):
         """
@@ -65,30 +64,30 @@ class Utility:
         :param list_of_data:
         :return:
         """
-        
+
         data = list(filter(lambda x: (x is not None) and x.get(key, "") in list_of_data, dict_data))
-        
+
         return data
-    
+
     @staticmethod
     def generate_ref_number(prefix: str = "BKT") -> str:
-        
+
         random_chars = secrets.token_hex(4).upper()
-        
+
         # Get current Year (last two digits) and Month
         now = datetime.today()
         yy = now.year
         mm = now.strftime("%m")
-        
+
         return f"{prefix}-{yy}{mm}-{random_chars}"
-    
+
     @staticmethod
     def get_offset(skip: int, limit: int):
         if skip <= 0:
             skip = 1
         offset = (skip - 1) * limit
         return offset
-    
+
     @staticmethod
     def get_product_status(stock: int, low_stock_threshold: int):
         """
@@ -102,13 +101,13 @@ class Utility:
         elif stock <= low_stock_threshold:
             return ProductStatusSchema.LOW_OF_STOCK
         return ProductStatusSchema.AVAILABLE
-    
+
     @staticmethod
     def validate_image_file_extension(file_name):
         allowed_extensions = ("jpeg", "jpg", "png", "webp")
         if not file_name.lower().endswith(allowed_extensions):
             raise DomainUnprocessableEntityError(message=f'File extension should be in {allowed_extensions}.')
-    
+
     @classmethod
     def is_file_uploaded(cls, img_file):
         """
@@ -130,9 +129,9 @@ class Utility:
                 except Exception as e:
                     # raise error if not valid
                     raise e
-        
+
         return is_file_upload
-    
+
     @staticmethod
     def format_order_number(order_id: str) -> str:
         """
@@ -141,23 +140,23 @@ class Utility:
         """
         year_today = datetime.today().year
         return f'BKT-{uuid4().hex[:4]}{year_today}-{order_id:08d}'
-    
+
     @staticmethod
     def get_cors():
         ip_add = ''
         if ConstantsData.ENVIRONMENT == EnvironmentStatus.Dev:
             # This is for CORS Middleware
             origins = [
-                    "http://localhost:3000",
-                    "http://127.0.0.1:3000",
-                    ]
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+            ]
         else:
             # for production
             origins = [
-                    'https://biskota-fwkhrpghr-john-pauls-projects-3d5afda2.vercel.app'
-                    ]
+                'https://biskota-eh60pdhi9-john-pauls-projects-3d5afda2.vercel.app/'
+            ]
         return origins
-    
+
     @staticmethod
     def get_paginated_data(*, offset, skip, limit, total_records) -> PaginatedOutput:
         """
@@ -169,9 +168,9 @@ class Utility:
         paginated_data = PaginatedOutput(start_page=curr_page, end_page=end_page,
                                          total_records=total_records,
                                          has_next=has_next)
-        
+
         return paginated_data if paginated_data.total_records else None
-    
+
     @classmethod
     def validate_birthdate(cls, birthdate: date):
         """
@@ -183,9 +182,9 @@ class Utility:
         user_age = cls.calculate_age(birthdate)
         if user_age < 18:
             DomainUnprocessableEntityError("Age must be at least 18 years old.")
-        
+
         return user_age
-    
+
     @staticmethod
     def calculate_age(birthday: date):
         """
@@ -197,32 +196,32 @@ class Utility:
         today_date = date.today()
         # and day,month and year to variables
         t_day, t_month, t_year = today_date.day, today_date.month, today_date.year
-        
+
         # assign day, month and year of user to variable
         user_b_day, user_b_month, user_b_year = birthday.day, birthday.month, birthday.year
-        
+
         # calculate age
         age = t_year - user_b_year
-        
+
         # check if today's month is less than to its birth month
         if t_month < user_b_month:
             age -= 1
-        
+
         # otherwise check today's month if equal to user's birthmonth
         elif t_month == user_b_month:
             # then check if today's day is less than to its birthday
             if t_day < user_b_day:
                 # then subtract 1 to its age
                 age -= 1
-        
+
         # handle zero and negative value
         if age <= 0:
             age = 1
         # then return age
         return age
-    
+
     @staticmethod
     def generate_otp_code():
         otp = str(secrets.randbelow(900000) + 100000)  # 6-digit
-        
+
         return otp
